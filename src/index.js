ddf561edced6a51bb799a269528526011dd20f31
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2020-01-05 16:58:58 +0200

    add 3 paragraph

--- src/index.js
+++ src/index.js
@@ -7,12 +7,35 @@ const App = () => {
   return (
     <div className="ui container comments ">
       <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
         <div className="content">
           <a href="/" className="auther">
-            sam       
+            sam
+          </a>
+
+          <div>
+            <span className="date">today at 4:00 am </span>
+          </div>
+          <div className="text">nice blog post</div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <div className="content">
+          <a href="/" className="auther">
+            sam
+          </a>
+
+          <div>
+            <span className="date">today at 4:00 am </span>
+          </div>
+          <div className="text">nice blog post</div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <div className="content">
+          <a href="/" className="auther">
+            sam
           </a>
 
           <div>
