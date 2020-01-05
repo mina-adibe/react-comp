c55f397d1f56f2e92ef195799d03cab987588823
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2020-01-05 23:54:04 +0200

    add 3 nested component

---
+++ src/cnn.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const cnn = () => {
+  return (
+    <div className="comment">
+      <div className="content">
+        <a href="/" className="auther">
+          sam
+        </a>
+
+        <div>
+          <span className="date">today at 4:00 am </span>
+        </div>
+        <div className="text">nice blog post</div>
+      </div>
+    </div>
+  );
+};
+
+export default cnn;
--- src/index.js
+++ src/index.js
@@ -1,49 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Cnn from "./cnn.js";
 
 import "./styles.css";
+import cnn from "./cnn.js";
 
 const App = () => {
   return (
     <div className="ui container comments ">
-      <div className="comment">
-        <div className="content">
-          <a href="/" className="auther">
-            sam
-          </a>
-
-          <div>
-            <span className="date">today at 4:00 am </span>
-          </div>
-          <div className="text">nice blog post</div>
-        </div>
-      </div>
-
-      <div className="comment">
-        <div className="content">
-          <a href="/" className="auther">
-            sam
-          </a>
-
-          <div>
-            <span className="date">today at 4:00 am </span>
-          </div>
-          <div className="text">nice blog post</div>
-        </div>
-      </div>
-
-      <div className="comment">
-        <div className="content">
-          <a href="/" className="auther">
-            sam
-          </a>
-
-          <div>
-            <span className="date">today at 4:00 am </span>
-          </div>
-          <div className="text">nice blog post</div>
-        </div>
-      </div>
+      <Cnn />
+      <Cnn />
+      <Cnn />
     </div>
   );
 };
